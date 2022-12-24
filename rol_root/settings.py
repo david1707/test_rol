f401483b077fc6fe863dfc36b4fad427eb314580
david1707 <DavidAlacant@gmail.com>
2022-12-24 12:28:18 +0100

    Custom users app added, DRF added to installed apps and configuration

--- rol_root/settings.py
+++ rol_root/settings.py
@@ -30,6 +30,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third party apps
+    "rest_framework",
+    # Local
+    "custom_users.apps.CustomUsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -114,3 +118,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Django Rest Framework configuration options
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ]
+}
